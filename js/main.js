eb1c89c6e42d9513eda2fa02c5d8462e0452c900
WangMou <hao131462@qq.com>
2019-07-05 14:32:13 +0800

    修改自适应窗口的banner
    因为发现大屏幕下，banner区域无法占满全屏，便写了一个js适应窗口高度。还有用CSS写了一个伸缩菜单栏

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // 用于首页头部导航栏的展开和关闭
 function openlist(){
 	var secondList=document.getElementById("secondList");
-	if(secondList.style.display=="none"){
-		secondList.style="display:block;";
-	}else{
-		secondList.style="display:none;";
-	}
-	
+	secondList.style="display:block;";
 }
 function closelist(){
 	 secondList.style="display:none;";
+}
+// 获得页面高度 
+window.onload=autoHeight;
+function autoHeight(){
+	var Height=window.innerHeight;
+	var banner=document.getElementsByClassName("banner")[0];
+	banner.style.height=Height+"px";
+	console.log("success");
 }
